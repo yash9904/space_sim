90e4cc429b9e81aca89daa4ac755f2648461e6ad
YashT02 <yashthakre839@gmail.com>
2022-11-04 16:23:09 +0530

    update

--- README.md
+++
@@ -1,4 +0,0 @@
-## Simulation of objects in space
-#### Tracking trajectories of two objects in space under the influence of gravitational force towards each other
-#### Preview:
-![image](https://user-images.githubusercontent.com/86224563/168233328-60de9240-87d2-4312-a6fc-ce9c442491db.png)
--- app.py
+++ app.py
@@ -1,27 +1,20 @@
-#%%
-import os
-import cv2
+import streamlit as st
 import numpy as np
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-from tqdm import tqdm
-from datetime import datetime
 
-mpl.rcParams['figure.max_open_warning'] = 0
+from time import sleep
+
+
 #%%
 def r(p, q):
     '''
-
     Parameters
     ----------
     p : Location of point p
     q : Location of point q
-
     Returns
     -------
     float
         Euclidean distance between the points
-
     '''
     d = np.sqrt(np.dot(p - q, (p - q).T))
     return d if d > 50 else 50
@@ -30,17 +23,14 @@ def ru(p, q):
     
     
     '''
-
     Parameters
     ----------
     p : Location of point p
     q : Location of point q
-
     Returns
     -------
     ndarray of floats
         unit vector pointing from p to q
-
     '''
     x = q[0] - p[0]
     y = q[1] - p[1]
@@ -49,24 +39,24 @@ def ru(p, q):
     
     return np.array([x / d, y / d])
 #%%
+
+
 mp = 4 * pow(10, 14)
 mq = 2 * pow(10, 14)
 
 G = 6.6743 * 10**(-11)
 
-up = [2, -3]
-uq = [4, 2]
 
-p = np.array([400.0, 400.0])
-q = np.array([300.0, 800.0])
+
+
 #%%
 def mass_circle_p(x, y):
     global mp, p
-    return ((x - p[0]) ** 2) + ((y - p[1]) ** 2) <= 400
+    return ((x - p[0]) ** 2) + ((y - p[1]) ** 2) <= 100
 
 def mass_circle_q(x, y):
     global mq, q
-    return ((x - q[0]) ** 2) + ((y - q[1]) ** 2) <= 200
+    return ((x - q[0]) ** 2) + ((y - q[1]) ** 2) <= 50
 
 size = (1000, 1000)
 space = np.zeros(size, np.float32)
@@ -80,30 +70,50 @@ del_t = 1
 n_iter = 1000
 
 #%%
-now = datetime.now().strftime("%d%m%Y_%H%M%S")
-frames_dir = 'frames_dir_space_sim' + now
 
-if not os.path.isdir(frames_dir):
-    os.mkdir(frames_dir)
+st.title('Objects in Space')
+st.sidebar.title('Configurations')
+px = st.sidebar.slider('x coordinate of first object', 0, size[0] - 1, value = 200)
+py = st.sidebar.slider('y coordinate of first object', 0, size[1] - 1, value = 200)
+p = np.array([px, py], dtype = np.int32)
 
-t = 2
-vid_dir = f'space_sim_t{del_t * n_iter}_{now}.mp4'
+qx = st.sidebar.slider('x coordinate of second object', 0, size[0] - 1, value = 500)
+qy = st.sidebar.slider('y coordinate of second object', 0, size[1] - 1, value = 600)
+q = np.array([qx, qy], dtype = np.int32)
+
+upu = st.sidebar.slider('x velocity of first object', -3, 3, value = 1)
+upv = st.sidebar.slider('y velocity of first object', -3, 3, value = 0)
+uqu = st.sidebar.slider('x velocity of second object', -3, 3, value = -1)
+uqv = st.sidebar.slider('y velocity of second object', -3, 3, value = 0)
+
+up = [upu, upv]
+uq = [uqu, uqv]
+
 
 #%%
-for i in tqdm(range(n_iter), desc = 'Simulating...'):    
+data_dict = {'Property': ['Mass (e14)(kg)', 'x-Velocity(m/s)', 'y-Velocity(m/s)'], 
+             'Object #1': [mp//10e13, up[0], up[1]],
+             'Object #2': [mq//10e13, uq[0], uq[1]]}
 
+st.sidebar.dataframe(data_dict)
+
+#%%
+widget = st.empty()
+t = 1
+while True:
+    sleep(0.01)
     ap = G * mq * ru(p, q) / (r(p, q)**2)
     aq = G * mp * ru(q, p) / (r(p, q)**2)
     
     up += (ap * del_t)
     uq += (aq * del_t)
     
-    p += (up * del_t)
-    q += (uq * del_t)
+    p += np.int32((up * del_t))
+    q += np.int32((uq * del_t))
     
     # Periodic boundary
-    p %= 1000
-    q %= 1000
+    p %= size[0]
+    q %= size[1]
 
     px, py = p.astype(np.uint16)
     qx, qy = q.astype(np.uint16)
@@ -113,27 +123,5 @@ for i in tqdm(range(n_iter), desc = 'Simulating...'):
     space[qx - t: qx + t, qy - t: qy + t] = 1
     
     new_space = space + update_space()
+    widget.image(new_space, clamp = True)
     
-    fig = plt.figure(figsize = (20, 15))
-    
-    plt.imshow(new_space, cmap = 'gray')
-    plt.xticks([])
-    plt.yticks([])
-    plt.savefig(frames_dir + "/img.{0:05d}.png".format(i))
-    plt.close(fig)
-
-imshape = cv2.imread(os.path.join(frames_dir, os.listdir(frames_dir)[0])).shape
-imshape = (imshape[1], imshape[0])
-
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-
-result = cv2.VideoWriter(vid_dir, 
-                         fourcc,
-                         20, imshape)
-
-for file in tqdm(sorted(os.listdir(frames_dir)), desc = "Saving ..."):
-    image = cv2.imread(os.path.join(frames_dir, file))
-    result.write(image)
-result.release()
-
-print(f'\nSimulation saved as: {vid_dir}')
